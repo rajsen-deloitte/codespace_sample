ea6227c3753767ceca3cc68d34bbb85033561253
Enrique Plata <domfp13@me.com>
2025-04-16 11:57:25 +0000

    This is adding both config and connection to entry

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,5 +4,10 @@
 if [ -f "$SNOWFLAKE_HOME/config.toml" ]; then
     chmod 0600 "$SNOWFLAKE_HOME/config.toml"
 fi
+
+if [ -f "$SNOWFLAKE_HOME/connections.toml" ]; then
+    chmod 0600 "$SNOWFLAKE_HOME/connections.toml"
+fi
+
 # Execute the container's main process
 exec "$@"
